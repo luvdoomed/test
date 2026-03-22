af2847f7df3a556d0dc9cea8b56e60f2ac937aee
luvdoomed <luvdoomed@gmail.com>
2026-03-22 23:44:46 +0300

    fix: deadlock при экспорте на длинных треках

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/theme.css'
 import './store/themeStore'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+)
