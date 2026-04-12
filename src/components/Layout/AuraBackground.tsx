6ca23a41711e73f572e5e43d12de504da57273af
luvdoomed <luvdoomed@gmail.com>
2026-04-12 18:03:54 +0300

    feat: AuraBackground блюр обложки

---
+++ src/components/Layout/AuraBackground.tsx
@@ -0,0 +1,9 @@
+
+export function AuraBackground() {
+  return (
+    <div className="aura" aria-hidden="true">
+      <div className="aura__blob aura__blob--1" />
+      <div className="aura__blob aura__blob--2" />
+    </div>
+  )
+}
